02e1e3cc1838688cb5ebbaa6296dfaf9501e8ad7
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-12 14:55:35 +0100

    init

---
+++ test.ps1
@@ -0,0 +1,51 @@
+﻿. $PSScriptRoot\translit.ps1
+
+
+$cTest = @(
+ 'велика лисица прескаче журно луњајући се љупко на џаку'
+)
+
+$cRes = @(
+ 'velika lisica preskače žurno lunjajući se ljupko na Džaku'
+)
+
+$lTest = @(
+ 'Ovom uredbom uređuju se izgled, sadržaj i način popunjavanja obrazaca, kao i način dostavljanja i obrade podataka koji se unose u Registar zaposlenih, izabranih, postavljenih i angažovanih lica u javnom sektoru.'
+ 'Uprava za trezor je sastavni deo Ministarstva finansija. Osnovana je 01. avgusta 2005. godine i nasledila je tadašnju Upravu za javna plaćanja i nekadašnji Zavod za obračun i plaćanja.'
+)
+
+$lRes =@(
+ 'Овом уредбом уређују се изглед, садржај и начин попуњавања образаца, као и начин достављања и обраде података који се уносе у Регистар запослених, изабраних, постављених и ангажованих лица у јавном сектору.'
+ 'Управа за трезор је саставни део Министарства финансија. Основана је 01. августа 2005. године и наследила је тадашњу Управу за јавна плаћања и некадашњи Завод за обрачун и плаћања.'
+)
+
+$i=0; $f=0; $g=0;
+
+$cTest | % {
+    $t = translit $_
+    $r = $cRes[$i]
+    if ($r -eq $t) { $g++} else {
+        "Test $i failed"
+        "  $t"
+        "  $r"
+        $f++ 
+    }
+    $i++
+}
+
+$i = 0;
+$lTest | % {
+    $t = translit $_ -To cyr
+    $r = $lRes[$i]
+    if ($r -eq $t) { $g++} else {
+        "Test $i failed"
+        "  $t"
+        "  $r"
+        $f++ 
+    }
+    $i++
+}
+
+"-"*40
+"$g tests ok"
+"$f tests failed"
---
+++ translit.ps1
@@ -0,0 +1,27 @@
+﻿function Load-Alphabet($alphabetName, $To) {
+    $alphabet = New-Object System.Collections.Specialized.OrderedDictionary
+    gc $PSScriptRoot\$alphabetName | % {
+        $a = $_ -split ' '
+        if ($To -eq 'lat') { $alphabet.Add($a[0], $a[1]) } else { $alphabet.Add($a[1], $a[0]) }
+    }
+
+    $alphabet
+}
+
+function Get-Transliteration {
+    [CmdletBinding()]
+    param(
+        #Text to be translated
+        [string] $Text,
+
+        #Direction of translation
+        [ValidateSet('lat', 'cyr')]
+        [string] $To = 'lat'
+       )
+
+    $alphabet = Load-Alphabet serbian $To
+    $alphabet.GetEnumerator() | % { $Text = $Text.Replace($_.Name, $_.Value) }
+    $Text
+}
+
+sal translit get-transliteration
