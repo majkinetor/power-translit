9e371623dd408a98487d06b5a3def8c7d994250a
Miodrag Milic <miodrag.milic@gmail.com>
2024-09-17 21:37:39 +0200

    Handle double chars differently

--- test.ps1
+++ test.ps1
@@ -1,22 +1,32 @@
-﻿. $PSScriptRoot\translit.ps1
+﻿. $PSScriptRoot\translit.ps1 -Force
 
 
 $cTest = @(
  'велика лисица прескаче журно луњајући се љупко на џаку'
-)
+ )
 
 $cRes = @(
- 'velika lisica preskače žurno lunjajući se ljupko na Džaku'
+ 'velika lisica preskače žurno lunjajući se ljupko na džaku'
 )
 
 $lTest = @(
  'Ovom uredbom uređuju se izgled, sadržaj i način popunjavanja obrazaca, kao i način dostavljanja i obrade podataka koji se unose u Registar zaposlenih, izabranih, postavljenih i angažovanih lica u javnom sektoru.'
  'Uprava za trezor je sastavni deo Ministarstva finansija. Osnovana je 01. avgusta 2005. godine i nasledila je tadašnju Upravu za javna plaćanja i nekadašnji Zavod za obračun i plaćanja.'
+ 'PHxLjGIFČUZS7znTmGTl'
+ 'džoker'
+ 'dŽoker'
+ 'Džoker'
+ 'DŽoker'
 )
 
 $lRes =@(
  'Овом уредбом уређују се изглед, садржај и начин попуњавања образаца, као и начин достављања и обраде података који се уносе у Регистар запослених, изабраних, постављених и ангажованих лица у јавном сектору.'
  'Управа за трезор је саставни део Министарства финансија. Основана је 01. августа 2005. године и наследила је тадашњу Управу за јавна плаћања и некадашњи Завод за обрачун и плаћања.'
+ 'ПХxЉГИФЧУЗС7знТмГТл',
+ 'џокер'
+ 'џокер'
+ 'Џокер'
+ 'Џокер'
 )
 
 $i=0; $f=0; $g=0;
@@ -24,11 +34,13 @@ $i=0; $f=0; $g=0;
 $cTest | % {
     $t = translit $_
     $r = $cRes[$i]
-    if ($r -eq $t) { $g++} else {
+    if ($r -ceq $t) { $g++} else {
         "Test $i failed"
-        "  $t"
-        "  $r"
-        $f++ 
+        "  input   $_"
+        "  trans   $t"
+        "  should  $r"
+        ""
+        $f++
     }
     $i++
 }
@@ -37,11 +49,13 @@ $i = 0;
 $lTest | % {
     $t = translit $_ -To cyr
     $r = $lRes[$i]
-    if ($r -eq $t) { $g++} else {
+    if ($r -ceq $t) { $g++} else {
         "Test $i failed"
-        "  $t"
-        "  $r"
-        $f++ 
+        "  input   $_"
+        "  trans   $t"
+        "  should  $r"
+        ""
+        $f++
     }
     $i++
 }
--- translit.ps1
+++ translit.ps1
@@ -1,16 +1,30 @@
 ﻿# Author: Miodrag Milic <miodrag.milic@gmail.com>
-# Last Change: 12-Feb-2016.
-# Version: 1.0
+# Last Change: 2024-09-17
+# Version: 1.1
 
 #requires -version 3.0
 
 function Load-Alphabet($alphabetName, $To) {
     $alphabet = New-Object System.Collections.Specialized.OrderedDictionary
-    gc $PSScriptRoot\$alphabetName | % {
+    Get-Content $PSScriptRoot\$alphabetName | % {
         $a = $_ -split ' '
-        if ($To -eq 'lat') { $alphabet.Add($a[0], $a[1]) } else { $alphabet.Add($a[1], $a[0]) }
+        if ($To -eq 'lat') {
+            $alphabet.Add($a[0], $a[1])
+        } else {
+            if ($a[1].Length -eq 2 -and [char]::IsUpper($a[1][0])) {
+                $alphabet.Add( $a[1].ToUpper(), $a[0] )
+                $alphabet.Add( [char]::ToLower($a[1][0]) + [char]::ToUpper($a[1][1]), $a[0].ToLower() )
+            }
+            $alphabet.Add($a[1], $a[0])
+        }
     }
 
+    if ($To -eq 'cyr') { # move double chars to the front
+        $global:alphabet2 = New-Object System.Collections.Specialized.OrderedDictionary
+        $alphabet.GetEnumerator() | % { if ($_.Name.Length -eq 2) { $alphabet2.Add($_.Name, $_.Value ) } }
+        $alphabet.GetEnumerator() | % { if ($_.Name.Length -eq 1) { $alphabet2.Add($_.Name, $_.Value ) } }
+        return $alphabet2
+    }
     $alphabet
 }
 
@@ -25,8 +39,8 @@ function Get-Transliteration {
         [string] $To = 'lat'
        )
 
-    $alphabet = Load-Alphabet serbian $To
-    $alphabet.GetEnumerator() | % { $Text = $Text.Replace($_.Name, $_.Value) }
+    $global:alphabet = Load-Alphabet serbian $To
+    $alphabet.GetEnumerator() | % { $Text = $Text.Replace($_.Name, $_.Value, $false, $null) }
     $Text
 }
 
