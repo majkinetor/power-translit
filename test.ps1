7eba09c4d8848ecd4c834714bd89d3094755db99
Miodrag Milic <miodrag.milic@gmail.com>
2024-09-17 21:39:44 +0200

    d

--- test.ps1
+++ test.ps1
@@ -13,6 +13,7 @@ $lTest = @(
  'Ovom uredbom uređuju se izgled, sadržaj i način popunjavanja obrazaca, kao i način dostavljanja i obrade podataka koji se unose u Registar zaposlenih, izabranih, postavljenih i angažovanih lica u javnom sektoru.'
  'Uprava za trezor je sastavni deo Ministarstva finansija. Osnovana je 01. avgusta 2005. godine i nasledila je tadašnju Upravu za javna plaćanja i nekadašnji Zavod za obračun i plaćanja.'
  'PHxLjGIFČUZS7znTmGTl'
+ 'PHxlJGIFČUZS7znTmGTl'
  'džoker'
  'dŽoker'
  'Džoker'
@@ -23,6 +24,7 @@ $lRes =@(
  'Овом уредбом уређују се изглед, садржај и начин попуњавања образаца, као и начин достављања и обраде података који се уносе у Регистар запослених, изабраних, постављених и ангажованих лица у јавном сектору.'
  'Управа за трезор је саставни део Министарства финансија. Основана је 01. августа 2005. године и наследила је тадашњу Управу за јавна плаћања и некадашњи Завод за обрачун и плаћања.'
  'ПХxЉГИФЧУЗС7знТмГТл',
+ 'ПХxљГИФЧУЗС7знТмГТл',
  'џокер'
  'џокер'
  'Џокер'
